c87121a6583b42af5d929840dfd492c56e86dd03
Olga Gromyko <olgagromyko@MacBook-Air-Olga.local>
2020-10-20 21:55:49 +0300

    fixed mistakes

--- 19.10.var2/script.js
+++ 19.10.var2/script.js
@@ -1,9 +1,10 @@
 for (i = 0; i < 101; i++) {
-var count1 = i % 5 === 0 && i % 3 === 0;
-    count2 = i % 3 === 0;
-    count3 = i % 5 === 0;
+var count1 = i === 0;
+    count2 = i % 5 === 0 && i % 3 === 0;
+    count3 = i % 3 === 0;
+    count4 = i % 5 === 0;
 
-    output = count1 ? "кратно 3 и 5" : count2 ? "кратно 3" : count3? "кратно 5" : i;
+    output = count1 ? "0" : count2 ? "кратно 3 и 5" : count3 ? "кратно 3" : count4 ? "кратно 5" : i;
 
     console.log(output);
 }
