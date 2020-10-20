f682bcb33a93ff664fcc9726e2eaf38c9922f2d8
Olga Gromyko <olgagromyko@MacBook-Air-Olga.local>
2020-10-20 21:50:11 +0300

    fixed mistakes

--- 19.10.var1/script.js
+++ 19.10.var1/script.js
@@ -1,18 +1,21 @@
 
 for (var i = 0; i <= 100;  i++)  {
-    if (i % 5 === 0 && i % 3 === 0) {
-        console.log("кратно 3 и кратно 5");
-    }  else {
-        if (i % 3 === 0) {
-            console.log("кратно 3");
-         }  else {
-                if (i % 5 === 0) {
-                console.log("кратно 5");
-                } else {
-                    console.log(i);
-                    }  
+    if (i === 0) {
+            console.log(0);
+        } else {
+            if (i % 5 === 0 && i % 3 === 0) {
+                console.log("кратно 3 и кратно 5");
+            }  else {
+                if (i % 3 === 0) {
+                    console.log("кратно 3");
+                }  else {
+                    if (i % 5 === 0) {
+                    console.log("кратно 5");
+                    } else {
+                        console.log(i);
+                        }  
+                }
             }
         }
     }
-
 
